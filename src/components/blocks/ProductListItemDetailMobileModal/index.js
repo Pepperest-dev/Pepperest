453f25b44a130f00f6c8323b3f85074531f5d09a
Simoncods <admin@uncensoredwears.com>
2020-09-09 18:09:24 +0100

    product id rm commit on 'deploy'

--- src/components/blocks/ProductListItemDetailMobileModal/index.js
+++ src/components/blocks/ProductListItemDetailMobileModal/index.js
@@ -8,7 +8,7 @@ import { getStringHash } from 'libs/utils';
 
 
 
-const ProductListItemDetailMobileModal = ({ productDetails : { productID,
+const ProductListItemDetailMobileModal = ({ productDetails : { 
   productName,
   dateCreated,
   deliveryDate,
